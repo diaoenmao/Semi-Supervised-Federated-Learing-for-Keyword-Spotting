d25dbba1fc44b65ae7fda585a3e2c1f92792470b
dem123456789 <diao_em@hotmail.com>
2022-07-07 04:00:23 -0400

    finalize

--- src/make.py
+++ src/make.py
@@ -81,7 +81,7 @@ def main():
         controls = make_controls(script_name, init_seeds, world_size, num_experiments, resume_mode, control_name)
     elif mode == 'ssfl':
         script_name = [['{}_classifier_ssfl.py'.format(run)]]
-        control_name = [[data, model, ['250', '2500'], ['basic=basic-spec'], ['fix-mix', 'fix'], ['100'], ['0.1'],
+        control_name = [[data, model, ['250', '2500'], ['basic=basic-spec'], ['fix-mix'], ['100'], ['0.1'],
                          ['iid', 'non-iid-l-2', 'non-iid-d-0.1', 'non-iid-d-0.3']]]
         controls = make_controls(script_name, init_seeds, world_size, num_experiments, resume_mode, control_name)
     else:
